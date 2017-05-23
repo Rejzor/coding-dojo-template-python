e207a4e32a1e78ad7a9acde89b449142c1cdc2e7
Rarez <209456@student.pwr.edu.pl>
2017-05-23 13:36:23 +0200

    Almost :)

---
+++ tests/basket.py
@@ -0,0 +1,39 @@
+class Basket():
+	pass
+
+def basket(a,b,c,d,e):	
+	books=[]
+	# cost1 = (int(sum(books))*8)*0.75
+	cost1=0
+
+	zerocounter=0
+	books.append(int(a))
+	books.append(int(b))
+	books.append(int(c))
+	books.append(int(d))
+	books.append(int(e))
+	sumbooks=sum(books)
+	while sumbooks > 0:
+		# How many zeros
+		for i in range(len(books)):
+			 if books[i] == 0:
+			 	zerocounter+=1
+	    #Diffrent discountes
+		if zerocounter==0:
+		 	cost1 += (int(5*8)*0.75)
+		elif zerocounter==1:
+			cost1+= (int(4*8)*0.8)
+		elif zerocounter==2:
+			cost1+=(int(3*8)*0.90)
+		elif zerocounter==3:
+			cost1+=(int(2*8)*0.95)
+		
+		#Decrement if not zero
+		for i in range(len(books)):
+			if books[i] != 0:
+				books[i]-=1
+		zerocounter=0
+		sumbooks=sum(books)
+	return print(cost1)
+basket(2,2,2,2,2)
+
--- tests/example.py
+++ tests/example.py
@@ -1,10 +1,24 @@
 import unittest
-from zad1 import suma
+class Basket():
+	def __init__(self):
+		self.count = 0
+	def counter(self):
+		return self.count
+	def addbook(self,id):
+		self.count += 1
+	def basketvalue(self):
+		return 8
 
 class ExampleTest(unittest.TestCase):
-    def test_example(self):
-    	res = suma(1,2)
-    	self.assertEqual(res, 3)
-
+    def test_emptybasket(self):
+    	basket = Basket()
+    def test_emptybasketcount(self):
+    	basket = Basket()
+    	self.assertEquals(0, basket.counter())
+    def test_addbook(self):
+    	basket = Basket()
+    	basket.addbook(1)
+    	self.assertEquals(1, basket.counter())
+    	self.assertEquals(8, basket.basketvalue())
 if __name__ == '__main__':
     unittest.main()
